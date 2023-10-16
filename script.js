ce31487a155f522a92dee2153eaa921a9e7c4236
Kuznetsov-100-Rads-Bar <ledrifterdepripyat@gmail.com>
2023-10-16 23:18:55 +0200

    Ajout fichier script.js pour la navigation

--- script.js
+++ script.js
@@ -2,4 +2,5 @@
 
 const nav = document.querySelector(".main-nav");
 const navToggler = document.querySelector(".nav-toggler");
-const togglerImg = navToggler.querySelector(".nav-toggler");
+const togglerImg = navToggler.querySelector("img");
+const navLinks = document.querySelectorAll(".main-nav a");
