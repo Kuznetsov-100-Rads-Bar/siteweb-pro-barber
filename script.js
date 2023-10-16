9111af4da395d711c3f006024d08f92f8e85c283
Kuznetsov-100-Rads-Bar <ledrifterdepripyat@gmail.com>
2023-10-16 23:34:40 +0200

    Ajout fichier script.js pour coder le script de la navbar

--- script.js
+++ script.js
@@ -10,6 +10,12 @@ navToggler.addEventListener("click", toggleNav);
 
 function toggleNav() {
   nav.classList.toggle("active");
+  if (togglerImg.src.includes("hamburger")) {
+    togglerImg.src = "ressources/cross.svg";
+  }
+  else {
+    togglerImg.src = "ressources/hamburger.svg";
+  }
 }
 
 
