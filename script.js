b775a6d31fc20b5c48ec36b3ba9262403f332da7
Kuznetsov-100-Rads-Bar <ledrifterdepripyat@gmail.com>
2023-10-23 19:35:11 +0200

    last commit

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // NAVIGATION
 
-
+const nav = document.querySelector(".main-nav");
+const navToggler = document.querySelector(".nav-toggler");
 const togglerImg = navToggler.querySelector("img");
 const navLinks = document.querySelectorAll(".main-nav a");
 
